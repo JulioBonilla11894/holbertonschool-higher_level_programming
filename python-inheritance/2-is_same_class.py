a589c71ebaf5f8a3114ca83494ca00e5232de93b
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2025-02-05 12:20:26 -0400

    fixing file 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
+"""This module contains the is_same_class function."""
+
 def is_same_class(obj, a_class):
+    """Returns True if obj is an instance of a_class."""
     return type(obj) is a_class
