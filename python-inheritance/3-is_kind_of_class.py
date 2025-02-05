c6910ccd40edec69057d537ae535d144ee1d0431
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2025-02-05 12:23:43 -0400

    fixing file 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""This module contains the is_kind_of_class function."""
+
 def is_kind_of_class(obj, a_class):
+    """Returns True if obj is an instance of a_class or if the obj is an instance of a class that inherited from a_class."""
     return isinstance(obj, a_class)
 
