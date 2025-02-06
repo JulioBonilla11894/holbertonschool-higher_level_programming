0a511c2d8b6a5baf19718fbe5e47ada6b4989eb8
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2025-02-06 08:43:04 -0400

    fixing file 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -27,6 +27,7 @@ class Rectangle(BaseGeometry)
     def area(self):
         """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
+
     def __str__(self):
         """Return the string representation of the rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
